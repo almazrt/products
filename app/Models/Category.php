880b8165ca9d50fc97322d4e21b6b260fd720f82
Сайфутдинов Алмаз Камилевич <asayfutdinov@phoenixit.ru>
2022-07-24 13:14:46 +0300

    add product-category relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     public const FIELD_ID = 'id';
     public const FIELD_TITLE = 'title';
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     public const FIELD_ID = 'id';
     public const FIELD_TITLE = 'title';
     public const FIELD_PRICE = 'price';
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
