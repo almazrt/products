5b69bedded1c28f5858be242627ca97266936f67
Сайфутдинов Алмаз Камилевич <asayfutdinov@phoenixit.ru>
2022-07-24 13:00:42 +0300

    add product

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public const FIELD_ID = 'id';
+    public const FIELD_TITLE = 'title';
+    public const FIELD_PRICE = 'price';
+}
